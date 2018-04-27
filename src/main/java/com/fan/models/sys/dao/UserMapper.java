271795e35b10260ea852eb3168f46aa48f9769b2
樊高风 <1224566146@qq.com>
2018-04-27 14:31:44 +0800

    将架构上传提交

---
+++ src/main/java/com/fan/models/sys/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.fan.models.sys.dao;
+
+import com.fan.models.sys.model.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(String userId);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(String userId);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/fan/models/sys/model/User.java
@@ -0,0 +1,96 @@
+package com.fan.models.sys.model;
+
+import java.util.Date;
+
+public class User {
+	//用户ID
+    private String userId;
+    //用户帐号
+    private String code;
+    //用户密码
+    private String password;
+    //用户姓名
+    private String userName;
+    //用户性别
+    private String sex;
+    //用户手机号
+    private String mobile;
+    //用户角色
+    private String roleId;
+    //审核状态
+    private String status;
+    //申请时间
+    private Date createTime;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId == null ? null : userId.trim();
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code == null ? null : code.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex == null ? null : sex.trim();
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId == null ? null : roleId.trim();
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status == null ? null : status.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+}
